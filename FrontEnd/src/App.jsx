de6c21bd088a29438bd4e7d1037030c0cd29d26f
REVANTH-2-0-0-4 <revanthkurapati56@gmail.com>
2025-03-01 18:19:38 +0530

    initial setup

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-     <div className='text-blue-500 text-xl'>hello</div>
+      <div className='bg-black min-h-screen w-full select-none'>
+        <div className=' h-screen p-4 w-full flex justify-center items-center flex-col'>
+          <div className='text-blue-500  tracking-loose tracking-tighter text-6xl font-["Nueue-Montrel"]'>COLLAB-CODE </div>
+          <div className='text-blue-900 pl-50'>....development in progress</div>
+        </div>
+      </div>
     </div>
   )
 }
