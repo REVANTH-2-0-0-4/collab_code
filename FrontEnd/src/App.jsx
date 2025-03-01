6641fa0600c032f560b977fe4926676099888092
Siddharth <siddharth2304p@gmail.com>
2025-03-01 17:38:12 +0530

    tailwind setup

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-     <div className='text-black'>hello</div>
+     <div className='text-blue-500 text-xl'>hello</div>
     </div>
   )
 }
